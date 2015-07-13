f6ee6e6accf496c338370d8a4694cd3035ae2477
gion <bogdan.gradinariu@thinslices.com>
2015-07-13 17:48:52 +0300

    yeoman scaffolding

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name allcalPhonegapTestApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the allcalPhonegapTestApp
+ */
+angular.module('allcalPhonegapTestApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name allcalPhonegapTestApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the allcalPhonegapTestApp
+ */
+angular.module('allcalPhonegapTestApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
