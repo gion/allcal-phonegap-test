c4a75a0548147793424c0cb37ff5d666484aaa39
gion <bogdan.gradinariu@thinslices.com>
2015-07-15 17:12:53 +0300

    desperate try

--- app/scripts/controllers/geolocation.js
+++ app/scripts/controllers/geolocation.js
@@ -12,66 +12,104 @@ angular.module('allcalPhonegapTestApp')
     var geolocation = this,
         timeout;
 
+    //
+    // var map;
+    // function initialize() {
+    //   map = new google.maps.Map(document.getElementById('map-canvas'), {
+    //     zoom: 8,
+    //     center: {lat: -34.397, lng: 150.644}
+    //   });
+    // }
+    //
+    // geolocation.loading = true;
+    // geolocation.error = null;
+    // geolocation.selectedAddress = null;
+    //
+    // geolocation.clickHandler = function(event) {
+    //   $timeout.cancel(timeout);
+    //   geolocation.loading = true;
+    //   geolocation.error = null;
+    //   geolocation.selectedAddress = null;
+    //
+    //   geocoder.reverseGeocode(event.latLng)
+    //     .then(function(result) {
+    //       // console.log('success', result);
+    //       geolocation.selectedAddress = result;
+    //     })
+    //     .catch(function(result) {
+    //       geolocation.selectedAddress = null;
+    //       // console.error('fail', result);
+    //       geolocation.error = result;
+    //     })
+    //     .finally(function() {
+    //       timeout = $timeout(function() {
+    //         geolocation.loading = false;
+    //       }, 500);
+    //     });
+    // };
+    //
+    // function init() {
+    //   console.log('phonegap reaaaaady!');
+    //
+    //   initialize();
+    //
+    //   $scope.$on('mapInitialized', function(event, map) {
+    //     geolocation.map = map;
+    //
+    //     console.log('mapInitialized', event, map);
+    //
+    //     geocoder.getCurrentPosition()
+    //       .then(function(position) {
+    //         geolocation.map.setCenter(position);
+    //         map.setCenter(position);
+    //       })
+    //       .catch(function(errorMessage) {
+    //         throw new Error(errorMessage);
+    //         geolocation.error = errorMessage;
+    //       })
+    //       .finally(function() {
+    //         geolocation.loading = false;
+    //       });
+    //   });
+    // };
+    //
+    // phonegapReady(init);
 
-    var map;
-    function initialize() {
-      map = new google.maps.Map(document.getElementById('map-canvas'), {
-        zoom: 8,
-        center: {lat: -34.397, lng: 150.644}
-      });
-    }
 
-    geolocation.loading = true;
-    geolocation.error = null;
-    geolocation.selectedAddress = null;
+    var key = 'AIzaSyB5a-L4dpBaneZ2oKfasJFWYw1-ez-wdP4';
 
-    geolocation.clickHandler = function(event) {
-      $timeout.cancel(timeout);
-      geolocation.loading = true;
-      geolocation.error = null;
-      geolocation.selectedAddress = null;
+    (function (global) {
+        "use strict";
 
-      geocoder.reverseGeocode(event.latLng)
-        .then(function(result) {
-          // console.log('success', result);
-          geolocation.selectedAddress = result;
-        })
-        .catch(function(result) {
-          geolocation.selectedAddress = null;
-          // console.error('fail', result);
-          geolocation.error = result;
-        })
-        .finally(function() {
-          timeout = $timeout(function() {
-            geolocation.loading = false;
-          }, 500);
-        });
-    };
+        function onDeviceReady () {
+          alert('device ready');
+            document.addEventListener("online", onOnline, false);
+            document.addEventListener("resume", onResume, false);
+            loadMapsApi();
+        }
 
-    function init() {
-      console.log('phonegap reaaaaady!');
+        function onOnline () {
+            loadMapsApi();
+        }
 
-      initialize();
+        function onResume () {
+            loadMapsApi();
+        }
 
-      $scope.$on('mapInitialized', function(event, map) {
-        geolocation.map = map;
+        function loadMapsApi () {
+          alert('try to load');
+            if(navigator.connection.type === Connection.NONE || google.maps) {
+                return;
+            }
+            $.getScript('https://maps.googleapis.com/maps/api/js?key='+ key +'&sensor=true&callback=onMapsApiLoaded');
+        }
 
-        console.log('mapInitialized', event, map);
+        global.onMapsApiLoaded = function () {
+          alert('yeah');
+            // Maps API loaded and ready to be used.
+            var map = new google.maps.Map(document.getElementById("map-canvas"), {});
+        };
 
-        geocoder.getCurrentPosition()
-          .then(function(position) {
-            geolocation.map.setCenter(position);
-            map.setCenter(position);
-          })
-          .catch(function(errorMessage) {
-            throw new Error(errorMessage);
-            geolocation.error = errorMessage;
-          })
-          .finally(function() {
-            geolocation.loading = false;
-          });
-      });
-    };
-
-    phonegapReady(init);
+        document.addEventListener("deviceready", onDeviceReady, false);
+    })(window);
   });
